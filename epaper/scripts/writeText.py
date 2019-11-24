e4c4b05745b93ff60096c086201990c8f9f8edc7
Mikael Labrut <labrut@gmail.com>
2019-11-24 12:55:13 +0100

    feat(epaper): writeText script

---
+++ epaper/scripts/writeText.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), '../node_modules/e-Paper/RaspberryPi&JetsonNano/python/pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), '../node_modules/e-Paper/RaspberryPi&JetsonNano/python/lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd2in7b
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("Write text on epd2in7b (e-paper 2.7 color screen)")
+    
+    epd = epd2in7b.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear()
+    time.sleep(1)
+    
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
+    
+    # Drawing on the Horizontal image
+    logging.info("1.Drawing on the Horizontal image...")
+    HBlackimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126
+    HRedimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126    
+    
+    drawblack = ImageDraw.Draw(HBlackimage)
+    drawred = ImageDraw.Draw(HRedimage)
+    drawblack.text((10, 0), 'hello world', font = font24, fill = 0)
+    drawblack.text((10, 20), sys.argv[1], font = font24, fill = 0)
+    drawblack.line((20, 50, 70, 100), fill = 0)
+    drawblack.line((70, 50, 20, 100), fill = 0)
+    drawblack.rectangle((20, 50, 70, 100), outline = 0)    
+    drawred.line((165, 50, 165, 100), fill = 0)
+    drawred.line((140, 75, 190, 75), fill = 0)
+    drawred.arc((140, 50, 190, 100), 0, 360, fill = 0)
+    drawred.rectangle((80, 50, 130, 100), fill = 0)
+    drawred.chord((200, 50, 250, 100), 0, 360, fill = 0)
+    epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRedimage))
+    time.sleep(2)
+    
+    logging.info("Goto Sleep...")
+    epd.sleep()
+        
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd2in7b.epdconfig.module_exit()
+    exit()
