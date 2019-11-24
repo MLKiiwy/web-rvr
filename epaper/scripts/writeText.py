78dd826a8dd018f4e9f40740e6af23e031ea5224
Mikael Labrut <labrut@gmail.com>
2019-11-24 12:58:59 +0100

    wip

--- epaper/scripts/writeText.py
+++ epaper/scripts/writeText.py
@@ -2,13 +2,14 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
+import logging
+
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), '../node_modules/e-Paper/RaspberryPi&JetsonNano/python/pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), '../node_modules/e-Paper/RaspberryPi&JetsonNano/python/lib')
 logging.info(libdir)
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
-import logging
 from waveshare_epd import epd2in7b
 import time
 from PIL import Image,ImageDraw,ImageFont
